575c8addf6bddcd7a038f36c7bc70cdc0f1796f4
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-02-25 13:56:30 +0100

    implements FFMPEG to fix video encoding

--- src/renderer/src/pages/Recorder.svelte
+++ src/renderer/src/pages/Recorder.svelte
@@ -16,6 +16,7 @@
     let audioEnabled = false;
     let timeRecording = 0;
     let panelOpened = true;
+    let processReady = true;
 
     $: sourceIndex = inputSources.findIndex(source => source.id === sourceId) + 1;
     $: formattedTime = formatTime(timeRecording);
@@ -34,16 +35,18 @@
             }
         };
 
-        navigator.mediaDevices
-            .getUserMedia(constraints as unknown)
-            .then(response => {
-                stream = response;
-            });
+        if (sourceId) {
+            navigator.mediaDevices
+                .getUserMedia(constraints as unknown)
+                .then(response => {
+                    stream = response;
+                });
+        }
     }
 
     async function startRecording(): Promise<void> {
         recording = true;
-        mediaRecorder = createMediaRecorder(stream, audioEnabled);
+        mediaRecorder = createMediaRecorder(stream, audioEnabled, () => { processReady = true; });
         mediaRecorder.start();
         createInterval(() => {
             ++timeRecording;
@@ -73,34 +76,39 @@
         start={startRecording}
         stop={() => {
             mediaRecorder?.stop();
+            processReady = false;
             timeRecording = 0;
             recording = false;
         }}
-        disabled={!sourceId}
+        disabled={!sourceId || !processReady}
         recording={recording}
     />
     <div class={`flex border border-slate-500 gap-1 transition-all items-center ${panelOpened ? 'w-[75%]' : 'w-[5%]'} rounded-md pr-1 py-[0.2rem] pl-2 bg-gradient-to-r from-slate-900 to-slate-950 relative z-10`}>
-        <div class={`${!panelOpened ? 'pointer-events-none ' : '' }relative`}>
-            <img src={audioEnabled ? audioIcon : muteIcon} alt="audio toggle" class={`scale-[0.9] w-6 h-6 relative top-[0.4px]${audioEnabled ? ' right-[0.75px]' : ''}`}/>
-            <label class={`${panelOpened ? 'cursor-pointer ' : ''}absolute w-3 h-3 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2`}>
-                <input checked={audioEnabled} type="checkbox" name="mic" on:change={updateAudio} hidden />
-            </label>
-        </div>
-        {#if !recording}
-            <div class={`${!panelOpened ? 'pointer-events-none ' : ''}flex items-center relative`}>
-                <p class="text-[0.5rem] leading-[1] bottom-0 text-slate-900 text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold pointer-events-none">{panelOpened ? sourceIndex : ''}</p>
-                <img src={screenIcon} alt="selected screen" class="w-6 h-6" />
-                <button class={`${panelOpened ? 'cursor-pointer' : 'cursor-default'} absolute place-items-center w-3 h-3 -translate-x-1/2 left-1/2`} on:click={() => openSecondaryWindow('screens')} disabled={!inputSources.length}>
-                </button>
+        {#if processReady}
+            <div class={`${!panelOpened ? 'pointer-events-none ' : '' }relative`}>
+                <img src={audioEnabled ? audioIcon : muteIcon} alt="audio toggle" class={`scale-[0.9] w-6 h-6 relative top-[0.4px]${audioEnabled ? ' right-[0.75px]' : ''}`}/>
+                <label class={`${panelOpened ? 'cursor-pointer ' : ''}absolute w-3 h-3 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2`}>
+                    <input checked={audioEnabled} type="checkbox" name="mic" on:change={updateAudio} hidden />
+                </label>
             </div>
-            <!-- <button class="cursor-pointer" on:click={() => openSecondaryWindow('settings')} disabled={recording}>
-                <Icon icon="settings" />
-            </button> -->
+            {#if !recording}
+                    <div class={`${!panelOpened ? 'pointer-events-none ' : ''}flex items-center relative`}>
+                        <p class="text-[0.5rem] leading-[1] bottom-0 text-slate-900 text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold pointer-events-none">{panelOpened ? sourceIndex : ''}</p>
+                        <img src={screenIcon} alt="selected screen" class="w-6 h-6" />
+                        <button class={`${panelOpened ? 'cursor-pointer' : 'cursor-default'} absolute place-items-center w-3 h-3 -translate-x-1/2 left-1/2`} on:click={() => openSecondaryWindow('screens')} disabled={!inputSources.length}>
+                    </button>
+                </div>
+                <!-- <button class="cursor-pointer" on:click={() => openSecondaryWindow('settings')} disabled={recording}>
+                    <Icon icon="settings" />
+                </button> -->
+            {:else}
+                <p class={`text-xs text-indigo-300 transition-opacity absolute right-2 ${panelOpened ? 'opacity-100' : 'opacity-0'}`}>{formattedTime}</p>
+            {/if}
+            <button class="z-20 cursor-pointer [&>*]:cursor-pointer absolute -right-3 grid content-center rounded size-4" on:click={togglePanel}>
+                <Icon icon="chevron" />
+            </button>
         {:else}
-            <p class={`text-xs text-indigo-300 transition-opacity absolute right-2 ${panelOpened ? 'opacity-100' : 'opacity-0'}`}>{formattedTime}</p>
+            <p class="text-indigo-300 text-xs w-full text-center">Saving</p>
         {/if}
-        <button class="z-20 cursor-pointer [&>*]:cursor-pointer absolute -right-3 grid content-center rounded size-4" on:click={togglePanel}>
-            <Icon icon="chevron" />
-        </button>
     </div>
 </section>
--- src/renderer/src/utils/record.ts
+++ src/renderer/src/utils/record.ts
@@ -1,25 +1,30 @@
 import { ipcRenderer } from 'electron';
 import nativeStream from 'stream';
-import FfmpegCommand from 'fluent-ffmpeg';
-import { path } from '@ffmpeg-installer/ffmpeg';
 import { deleteInterval } from './'; 
+import log from 'electron-log/renderer';
+
+// @ts-ignore: Importing as ES6 doesn't work
+const FfmpegCommand = require('fluent-ffmpeg');
+// @ts-ignore: Importing as ES6 doesn't work
+const { path: ffmpegPath } = require('@ffmpeg-installer/ffmpeg');
 
 let recordedChunks: Blob[] = [];
 
-export function createMediaRecorder(stream: MediaStream, audioEnabled: boolean) {
+export function createMediaRecorder(stream: MediaStream, audioEnabled: boolean, onFinished: () => void) {
     const options = {
         mimeType: 'video/webm; codecs=vp9'
     };
     stream.getAudioTracks()[0].enabled = audioEnabled;
     const mediaRecorder = new MediaRecorder(stream, options);
     mediaRecorder.ondataavailable = onDataAvailable;
-    mediaRecorder.onstop = stopRecording;
+    mediaRecorder.onstop = () => stopRecording(onFinished);
     return mediaRecorder;
 };
 
 function loadFfmpeg() {
     const ffmpeg = FfmpegCommand();
-    ffmpeg.setFfmpegPath(path.replace('app.asar', 'app.asar.unpacked'));
+    const ffmpegStaticPath = ffmpegPath.replace('app.asar', 'app.asar.unpacked');
+    ffmpeg.setFfmpegPath(ffmpegStaticPath);
 
     return ffmpeg;
 }
@@ -36,22 +41,31 @@ function onDataAvailable(e: BlobEvent): void {
     recordedChunks.push(e.data);
 }
 
-async function stopRecording(): Promise<void> {
+async function stopRecording(callback: () => void): Promise<void> {
     deleteInterval();
     const date_time = /(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})/.exec(new Date().toISOString());
     const filename = `${date_time[1]}_${date_time[2].replace(/:/g, '-')}`;
     const blob = new Blob(recordedChunks, { type: 'video/webm; codecs=vp9' });
     const buffer = Buffer.from(await blob.arrayBuffer());
+    const ffmpeg = loadFfmpeg();
+    const readableVideoBuffer = createReadableVideoBuffer(buffer);
 
     const { canceled, filePath } = await ipcRenderer.invoke('SHOW_SAVE_DIALOG', { name: filename, format: 'mp4' });
-    if (canceled) return;
-
-    if (filePath) {
-        const ffmpeg = loadFfmpeg();
-        const readableVideoBuffer = createReadableVideoBuffer(buffer);
-
+    if (canceled) return callback();
+    else if (filePath) {
         ffmpeg
             .input(readableVideoBuffer)
+            .videoCodec('libx264')
+            .audioCodec('libmp3lame')
+            .outputOptions('-max_muxing_queue_size 9999')
+            .on('error', function(err: Error) {
+                log.error(err.message);
+                callback();
+            })
+            .on('end', function() {
+                console.log('Processing finished!');
+                callback();
+            })
             .output(filePath)
             .run();
     }
